26ef1f36f1d483fc8740989991bafd5779582d9d
alexshavkunov <aleeexshavkunov@gmail.com>
2024-12-16 18:46:07 +0500

    Initial Commit

---
+++ russianStressTest/russianStressTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  russianStressTestApp.swift
+//  russianStressTest
+//
+//  Created by Alex Shavkunov on 16.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct russianStressTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
